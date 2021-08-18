61387105e4291404972b798746b805489aaaa3a8
iCode_Rx <arababs2015@gmail.com>
2021-08-18 11:46:26 +0100

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from "react-dom"
+
+function Navbar() {
+          return (
+                    <div>
+                              <div className="navbar">
+                                        <Link  to="#" className="menu-bars">
+
+                                        </Link>
+                              </div>
+                    </div>
+          )
+}
+
+export default Navbar
+
+// npm install react-icons --save
+
+// https://react-icons.github.io/react-icons/
